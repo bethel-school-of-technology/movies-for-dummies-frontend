7ad2adc6401c0c9870bf93fea54789c9dc854a5c
matparham <matparham@gmail.com>
2020-09-03 21:14:48 -0500

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,35 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanetsComponent } from './components/planets/planets.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { MovieDetailsComponent } from './movie/movie-details/movie-details.component';
+import { MovieAddComponent } from './movie/movie-add/movie-add.component';
+import { MovieEditComponent } from './movie/movie-edit/movie-edit.component';
+import { PostComponent } from './post/post.component';
+import { PostDetailsComponent } from './post/post-details/post-details.component';
+import { PostAddComponent } from './post/post-add/post-add.component';
+import { PostEditComponent } from './post/post-edit/post-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlanetsComponent
+    PlanetsComponent,
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    MovieAddComponent,
+    MovieEditComponent,
+    PostComponent,
+    PostDetailsComponent,
+    PostAddComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule,
